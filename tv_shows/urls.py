51167bdb5bbf24240c4f21acf87cabff0232c983
pc <maksimseredin8386@gmail.com>
2023-10-27 21:21:00 +0600

    'crud'

--- tv_shows/urls.py
+++ tv_shows/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('',views.tvShowListView),
     path('show_detail/<int:id>/',views.tvShowDetailView),
+    path('delete_list/', views.deleteListView),
+    path('update_list/', views.updateListView),
+    path('update_list/<int:id>/update/', views.editFilmView),
+    path('delete_list/<int:id>/delete/', views.dropFilmView),
+    path('create_film/', views.createShowView),
+
 ]
--- tv_shows/views.py
+++ tv_shows/views.py
@@ -1,13 +1,69 @@
 from django.shortcuts import render, get_object_or_404
-from . import models
+from . import models, forms
+from django.http import HttpResponse
+def createShowView(request):
+
+    if request.method == 'POST':
+        showForm = forms.TvShowForm(request.POST, request.FIELS)
+        if showForm.is_valid()
+            showForm.save()
+            return HttpResponse('Успешно добавлен <a href="/"> На главную </a> ')
+    else:
+        showForm = forms.TvShowForm()
+    return  render(request, template_name:'crud/create_film.html', context: {'from': showForm})
+
+def deleteListView(request):
+    if request.method == 'GET':
+        show = models.TvShow.objects.all()
+        context = {
+            'show_key': show,
+        }
+        html_name = 'crud/del_film_list.html'
+        return render(request, html_name, context)
+
+
+def dropFilmView(request, id):
+    film_id = get_object_or_404(models.TvShow, id=id)
+    film_id.delete()
+    return HttpResponse('Успешно удален <a href="/"> На главную </a> ')
+
+def editFilmView(request, id):
+    film_id = get_object_or_404(models.TvShow, id=id)
+    if request.method == 'POST':
+        form = forms.TvShowForm(instance=film_id, data=request.POST)
+        if forms.is_valid():
+            form.save()
+            return HttpResponse('Успешно изменен <a href="/"> На главную </a> ')
+    else:
+        from = forms.TvShowForm(instance=film_id)
+    context = {
+        'film_key':film_id,
+        'form':form,
+    }
+    return render(request, template_name:'crud/edit.html', context)
+
+
+
+def updateListView(request):
+        if request.method == 'GET':
+            show = models.TvShow.objects.all()
+            context = {
+                'show_key': show,
+            }
+            html_name = 'crud/del_film_list.html'
+            return render(request, html_name, context)
+
+
+
 
 def tvShowListView(request):
-    show = models.TvShow.objects.all()
-    context = {
-        'show_key': show,
-    }
-    html_name = 'tvshow/tvshow.html'
-    return render(request, html_name, context)
+    if request.method == 'GET':
+        show = models.TvShow.objects.all()
+        context = {
+            'show_key': show,
+        }
+        html_name = 'tvshow/tvshow.html'
+        return render(request, html_name, context)
 
 def tvShowDetailView(request, id):
     show_id = get_object_or_404(models.TvShow, id=id)
